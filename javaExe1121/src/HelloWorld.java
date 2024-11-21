a5241d923954ac40084319736fef15681d9c3003
TANAKA　AZUSA <108647629+tanaka-azusa404@users.noreply.github.com>
2024-11-21 13:17:56 +0900

    練習用のテンプレートをアップロードしました

---
+++ javaExe1121/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		String japanese = "こんにちは";
+		messege(japanese);
+		
+		System.out.println("終了");
+	}
+	
+	public static void messege(String msg) {
+		System.out.println(msg);
+	}
+
+}
